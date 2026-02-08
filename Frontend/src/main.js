fd331b2854e5a4355e9c2b518e20c87d93157d76
jaelg1 <jaelgamarra15@gmail.com>
2026-02-08 19:52:18 -0300

    errores tokens

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,18 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import '@/assets/global.css'
+
+// Enviar el token en todas las peticiones a la API cuando exista
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
 
 createApp(App).use(router).mount('#app')
