88253fee144e301040cd02b48f0eb950c704e10d
jaelg1 <jaelgamarra15@gmail.com>
2026-02-08 18:53:26 -0300

    favicon

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Smart Assistant'
+    }
+  },
   transpileDependencies: true,
   devServer: {
     port: 8081, // Puerto que coincide con la configuración CORS del backend
